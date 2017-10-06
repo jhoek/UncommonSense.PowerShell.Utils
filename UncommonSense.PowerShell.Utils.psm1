438eaeb7c2aec21fd12e599767180e8c0c8f21be
Jan Hoek <github@uncommonsense.nl>
2017-10-06 08:22:21 +0200

    Initial commit.

---
+++ UncommonSense.PowerShell.Utils.psm1
@@ -0,0 +1,32 @@
+function Format-HashTable 
+{
+	param
+	(
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+		[System.Collections.IDictionary[]]$HashTable,
+		
+		[ValidateRange(0, [int]::MaxValue)]
+		[int]$Indentation = 0
+	)
+	
+	process
+	{
+        foreach($Item in $HashTable)
+        {
+		    foreach($Key in $Item.Keys)
+		    {
+			    $Value = $Item["$Key"]
+		
+			    if ($Value -is [hashtable] -or $Value -is [System.Collections.Specialized.OrderedDictionary])
+			    {
+                    Write-Output "$("  " * $Indentation)$Key"
+				    Format-HashTable -HashTable $Value -Indentation ($Indentation + 1)
+			    }
+			    else
+			    {
+				    Write-Output "$("  " * $Indentation)$Key = $Value"
+			    }	
+		    }
+        }
+	}
+}
