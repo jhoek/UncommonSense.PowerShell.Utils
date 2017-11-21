a2a42697c963a230e6c5a72d097932c1e062ab2b
Jan Hoek <github@uncommonsense.nl>
2017-11-21 12:15:02 +0100

    Added Format-Object function

--- UncommonSense.PowerShell.Utils.psm1
+++ UncommonSense.PowerShell.Utils.psm1
@@ -1,3 +1,45 @@
+Set-StrictMode -Version Latest
+
+function Format-Object 
+{
+    param
+    (
+        [Parameter(Mandatory)][string]$Caption,
+        [Parameter(ValueFromPipeline)][PSObject[]]$Value,
+        [int]$Indentation = 0
+    )
+
+    process 
+    {
+        foreach($Item in $Value)
+        {
+            switch($true) 
+            {
+                # Hashtable
+                ($Item -is [System.Collections.IDictionary]) 
+                {
+                    Write-Output "$("  " * $Indentation)$Caption" 
+                    $Item.Keys | Foreach-Object { Format-Object -Caption $_ -Value $Item[$_] -Indentation ($Indentation + 1) } 
+                    break
+                }
+
+                # Collection
+                ($Item -is [System.Collections.ICollection]) 
+                { 
+                    Write-Output "$("  " * $Indentation)$Caption" 
+                    $Item | ForEach-Object { Format-Object -Caption $_.ToString() -Value $_ -Indentation ($Indentation + 1) }                                         
+                    break 
+                }
+
+                default 
+                {
+                    Write-Output "$("  " * $Indentation)$($Caption)=$($Item.ToString())"
+                }
+            }
+        }
+    }
+}
+
 function Format-HashTable 
 {
 	param
