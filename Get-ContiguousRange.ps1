4c5f095dce78995e2645cde6b6d2557502c073e8
Jan Hoek <github@uncommonsense.nl>
2018-06-19 15:14:27 +0200

    Moved example; made pipeline-aware

--- Get-ContiguousRange.ps1
+++ Get-ContiguousRange.ps1
@@ -1,26 +1,36 @@
 # .SYNOPSIS
 # Returns the first item of the first contiguous range of items of the specified length
-function Get-ContiguousRange
-{
+# .EXAMPLE
+# 1..20 | ForEach-Object { Get-ContiguousRange -Items (1..9 + 25..35 + 50..90) -RequiredLength $_ }
+function Get-ContiguousRange {
     [CmdletBinding()]
     Param
     (
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ValueFromPipeline)]
         [int[]]$Items,
 
         [ValidateRange(1, [int]::MaxValue)]
         [int]$RequiredLength = 1
     )
 
-    $Index = 0
+    begin {
+        $CachedItems = New-Object TypeName System.Collections.ArrayList
+    }
 
-    $Items | `
-        ForEach-Object { [pscustomobject]@{ Difference = $_ - $Index; Value = $_; Index = $Index++ } } | `
-        Group-Object -Property Difference | `
-        Where-Object -Property Count -GE $RequiredLength | `
-        Select-Object -First 1 -ExpandProperty Group |
-        Select-Object -First 1 -ExpandProperty Value
+    process {
+        foreach ($Item in $Items) {
+            $CachedItems.Add($Item)
+        }
+    }
+
+    end {
+        $Index = 0
+
+        $CachedItems | `
+            ForEach-Object { [pscustomobject]@{ Difference = $_ - $Index; Value = $_; Index = $Index++ } } | `
+            Group-Object -Property Difference | `
+            Where-Object -Property Count -GE $RequiredLength | `
+            Select-Object -First 1 -ExpandProperty Group |
+            Select-Object -First 1 -ExpandProperty Value
+    }
 }
-
-# e.g. 
-1..20 | ForEach-Object { Get-ContiguousRange -Items (1..9 + 25..35 + 50..90) -RequiredLength $_ }
