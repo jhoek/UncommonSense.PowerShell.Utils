1fc0430e58fd5a6b64ddf652783a8816ac9b7681
Jan Hoek <github@uncommonsense.nl>
2018-11-06 12:40:16 +0100

    Updated Psake build script

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,4 +1,13 @@
-Task Default -Depends UpdateReadMe
+Properties {
+    $ManifestPath = Get-ChildItem -Path "$PSScriptRoot/*.psd1" | Select-Object -First 1 -ExpandProperty FullName
+    $PublicFunctions = Get-ChildItem -Path "$PSScriptRoot/Public/*.ps1" | Select-Object -ExpandProperty BaseName
+}
+
+Task Default -Depends UpdateManifest, UpdateReadMe
+
+Task UpdateManifest {
+    Update-ModuleManifest -Path $ManifestPath -FunctionsToExport $PublicFunctions
+}
 
 Task UpdateReadMe {
     .\Update-README.ps1
