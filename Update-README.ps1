c168d1d804550da0e1af7555d60a5217aea704cb
Jan Hoek <github@uncommonsense.nl>
2018-11-30 11:36:05 +0100

    Updated build file; integrated Update-README.ps1

--- Update-README.ps1
+++
@@ -1,6 +0,0 @@
-Import-Module (Join-Path $PSScriptRoot UncommonSense.PowerShell.Utils.psd1) -Force
-
-Convert-HelpToMarkdown `
-    -Title UncommonSense.PowerShell.Utils `
-    -Commands (Get-Command -Module UncommonSense.PowerShell.Utils) `
-    -Description 'PowerShell utility cmdlets' | Out-File -FilePath (Join-Path $PSScriptRoot README.md) -Encoding utf8
--- psakefile.ps1
+++ psakefile.ps1
@@ -1,14 +1,29 @@
 Properties {
+    $ModuleName = Get-ChildItem -Path "$PSScriptRoot/*.psd1" | Select-Object -First 1 -ExpandProperty BaseName
     $ManifestPath = Get-ChildItem -Path "$PSScriptRoot/*.psd1" | Select-Object -First 1 -ExpandProperty FullName
     $PublicFunctions = Get-ChildItem -Path "$PSScriptRoot/Public/*.ps1" | Select-Object -ExpandProperty BaseName
 }
 
-Task Default -Depends UpdateManifest, UpdateReadMe
+Task Default -Depends UpdateExportedFunctions, UpdateReadMe
 
-Task UpdateManifest {
+Task UpdateExportedFunctions {
     Update-ModuleManifest -Path $ManifestPath -FunctionsToExport $PublicFunctions
 }
 
 Task UpdateReadMe {
-    .\Update-README.ps1
+    Import-Module (Join-Path $PSScriptRoot UncommonSense.PowerShell.Utils.psd1) -Force
+
+    Convert-HelpToMarkdown `
+        -Title $ModuleName `
+        -Commands (Get-Command -Module $ModuleName) `
+        -Description 'PowerShell utility cmdlets' |
+        Out-File -FilePath (Join-Path $PSScriptRoot README.md) -Encoding utf8
+}
+
+Task BumpVersionNo {
+    Update-ModuleManifest -Path $ManifestPath -ModuleVersion (Get-ModuleVersion -Path $ManifestPath | Step-ModuleVersion -By Build)
+}
+
+Task Publish -Depends BumpVersionNo, UpdateExportedFunctions, UpdateReadMe {
+    Publish-Module -Path . -NuGetApiKey $env:NuGetApiKey
 }
