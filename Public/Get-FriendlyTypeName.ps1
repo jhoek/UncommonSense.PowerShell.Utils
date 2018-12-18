9b0954f348f251c6eff4181abacff279fedcd45f
Jan Hoek <github@uncommonsense.nl>
2018-12-18 08:28:11 +0100

    Remove data types not supported in PowerShell Core

--- Public/Get-FriendlyTypeName.ps1
+++ Public/Get-FriendlyTypeName.ps1
@@ -1,7 +1,7 @@
-function Get-FriendlyTypeName 
+function Get-FriendlyTypeName
 {
     [OutputType([string])]
-    param    
+    param
     (
         [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
         [Type[]]$Type,
@@ -29,15 +29,11 @@ function Get-FriendlyTypeName
             [int]     = 'int'
             [long]    = 'long'
             [sbyte]   = 'sbyte'
-            [short]   = 'short'
             [string]  = 'string'
-            [uint]    = 'uint'            
-            [ushort]  = 'ushort'
-            [ulong]   = 'ulong'
             [void]    = 'void'
         }
     }
-    process    
+    process
     {
         $Type.ForEach{
             $CurrentType = $_
@@ -50,7 +46,7 @@ function Get-FriendlyTypeName
                     break
                 }
 
-                ($CurrentType.IsArray) 
+                ($CurrentType.IsArray)
                 {
                     $ArrayFormat -f ($CurrentType.GetElementType() | Get-FriendlyTypeName)
                     break
@@ -62,7 +58,7 @@ function Get-FriendlyTypeName
                     break
                 }
 
-                ($CurrentType.IsGenericType) 
+                ($CurrentType.IsGenericType)
                 {
                     $BaseName = $CurrentType.Name -replace '`.*$', ''
                     $Parameters = $CurrentType.GetGenericArguments() | Get-FriendlyTypeName
@@ -70,9 +66,9 @@ function Get-FriendlyTypeName
                     break
                 }
 
-                default 
-                { 
-                    $CurrentType.Name 
+                default
+                {
+                    $CurrentType.Name
                 }
             }
         }
