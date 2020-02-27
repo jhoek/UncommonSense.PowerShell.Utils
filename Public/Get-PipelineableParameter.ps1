02787faae11f36cb092c46997b78f9b70ad2f73a
Jan Hoek <github@uncommonsense.nl>
2020-02-27 13:22:49 +0100

    Function for retrieving pipelineable parameters for a command

---
+++ Public/Get-PipelineableParameter.ps1
@@ -0,0 +1,42 @@
+function Get-PipelineableParameter
+{
+    param
+    (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
+        [string[]]$Name
+    )
+
+    process
+    {
+        $Name.ForEach{
+            Get-Command -Name $_ `
+            | ForEach-Object {
+                $CurrentCommand = $_
+
+                $CurrentCommand `
+                | Select-Object -ExpandProperty Parameters `
+                | Select-Object -ExpandProperty Values `
+                | ForEach-Object {
+                    $CurrentParameter = $_
+
+                    $CurrentParameter `
+                    | Select-Object -ExpandProperty Attributes `
+                    | Where-Object { $_.ValueFromPipeline -or $_.ValueFromPipelineByPropertyName } `
+                    | ForEach-Object {
+                        [PSCustomObject]@{
+                            CommandName                     = $CurrentCommand.Name
+                            ParameterSetName                = $_.ParameterSetName
+                            ParameterName                   = $CurrentParameter.Name
+                            ParameterType                   = $CurrentParameter.ParameterType
+                            ValueFromPipeline               = $_.ValueFromPipeline
+                            ValueFromPipelineByPropertyName = $_.ValueFromPipelineByPropertyName
+                            PSTypeName                      = 'UncommonSense.PowerShell.Utils.PipelineableParameter'
+                        }
+                    }
+            }
+    }
+}
+}
+}
+
+Get-Command Show-AlpsObject | Get-PipelineableParameter
